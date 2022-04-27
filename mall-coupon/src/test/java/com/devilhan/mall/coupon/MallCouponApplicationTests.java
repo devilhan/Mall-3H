ff7b89eb0b5e82c47f2c13cf257e3bc8e220d3a0
hanyanjiao <hanyanjiao@tedu.cn>
2022-04-27 16:38:50 +0800

    init project

---
+++ mall-coupon/src/test/java/com/devilhan/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,11 @@
+package com.devilhan.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ mall-order/src/test/java/com/devilhan/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,11 @@
+package com.devilhan.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
